341d63e60b0ad4fd1618469a868cca1df168c292
ravibs298 <ravibs2908@gmail.com>
2025-09-09 15:32:13 +0530

    JS WORK1

---
+++ work1.js
@@ -0,0 +1,21 @@
+//output method
+console.log("FSD CLASS");
+
+//input method
+//let a = prompt("enter a nos:-");
+//console.log(a);
+var a = 20;
+var a = 30;//can be redeclare
+var a = 40;
+
+let b = 30;
+//let b = 38;// cant redeclare 
+b =70;
+{
+    let name = "NIPUN";
+    name = "XYZ";
+    console.log("name: " + name);
+    b = 659;
+    console.log("AFTER REASSIGNMENT OF B VALUE: " + b);
+
+}
