8ccc2e84bc5022a533e923df742a48d00b6a2605
ravibs298 <ravibs2908@gmail.com>
2025-09-10 09:24:35 +0530

    Work1 js

--- work1.js
+++ work1.js
@@ -12,10 +12,37 @@ let b = 30;
 //let b = 38;// cant redeclare 
 b =70;
 {
-    let name = "NIPUN";
+    let name = "RAVI";
     name = "XYZ";
     console.log("name: " + name);
     b = 659;
     console.log("AFTER REASSIGNMENT OF B VALUE: " + b);
-
 }
+
+// string
+let firstname = "RAVI";
+let lastname = "SINGH";
+let message = "HELLO";
+let outputMessage = message + "," + firstname + " " + lastname + "!";
+console.log(outputMessage);
+
+let sum = "22" + 2 - 2;
+console.log(sum);
+
+//typechecking
+console.log(typeof(sum));
+
+//implicit type
+let op = "22" - 2;
+console.log(op + " type of op--: " + typeof(op));
+
+let strToNum = "Hey" - 2;
+console.log(strToNum + " " + typeof(strToNum));
+
+const student = {
+    rollno : 234,
+    firstName :"RAVI ",
+    lastName :"SINGH",
+}
+console.log(typeof(student));
+console.log(student[0]['firstName'])
